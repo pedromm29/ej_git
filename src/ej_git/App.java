14413a36f77412c0c377f17b4f40b1de3758502c
Pedro <moralmolinap29@gmail.com>
2025-02-28 20:30:27 +0100

    Creado array de palabras

--- src/ej_git/App.java
+++ src/ej_git/App.java
@@ -4,6 +4,12 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		
+		String [] palabras = {"barca", "berrinche", "bomba", "buñuelo"};
+		
+		for (String pal: palabras) {
+			System.out.println(pal);
+		}
 
 	}
 
